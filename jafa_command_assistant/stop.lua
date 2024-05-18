cb8aa7fe8aa2118c03db7a23aa22953abb9d7191
Jafacakes10 <154052802+Jafacakes10@users.noreply.github.com>
2024-05-18 12:26:28 +0100

    Delete jafa_command_assistant directory

--- jafa_command_assistant/stop.lua
+++
@@ -1,8 +0,0 @@
-minetest.register_chatcommand("stop",{
-    description = "This will shutdown the server",
-    params = "Reason",
-    privs = {server = true},
-    func = function(name, params)
-        minetest.request_shutdown(" - This server has been forcably closed by: " ..name.. " - " .. " For the reason: " ..params)
-    end,
-})
